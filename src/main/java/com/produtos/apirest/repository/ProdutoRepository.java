12bb62583163abfc1bed50d33b360ad5800cf725
Leandro Amorim <leandroamorimpb@gmail.com>
2021-06-14 08:08:03 -0300

    :package: Criando e configurando o model, repository e o arquivo application.properties

---
+++ src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
@@ -0,0 +1,14 @@
+package com.produtos.apirest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.produtos.apirest.models.Produto;
+
+/**
+ * Possui vários métodos prontos para realizar persistência no banco de dados
+ * @author Leandro
+ *
+ */
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
